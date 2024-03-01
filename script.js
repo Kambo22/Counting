1e743a14d32b9300332e37cb6855e7ccb85d4c02
Kambo22 <anikolov432@gmail.com>
2024-03-01 12:08:57 +0200

    Random Number color

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 
 let count = 0;
+    const countTextElement = document.getElementById('countText');
+    const nextNumberElement = document.getElementById('nextNumber');
 
+    // Function to update the count and the "Next number"
+    function updateCount() {
+      count++;
+      // Updates the the count element
+      countTextElement.textContent = count;
+      // Updates the "Next number" element
+      nextNumberElement.textContent = count + 1;
+      // Assigns a random color to the count text
+      countTextElement.style.color = getRandomColor();
+      updateBackground();
+    }
 
-const countTextElement = document.getElementById('countText');
+    // Function that generates a random color
+    function getRandomColor() {
+      return '#' + Math.floor(Math.random()*16777215).toString(16);
+    }
 
-const nextNumberElement = document.getElementById('nextNumber');
+    // Function that updates the background color
+    function updateBackground() {
+      // Generates a random color for the background
+      const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+      // Sets the background color of body
+      document.body.style.backgroundColor = randomColor;
+    }
 
-// Function to update the count and the "Next number"
-function updateCount() {
-  count++;
-  // Updates the the count element
-  countTextElement.textContent = count;
-  // Updates the "Next number" element
-  nextNumberElement.textContent = count + 1;
-  updateBackground();
-}
-
-// Function that updates the background color
-function updateBackground() {
-  // Array of colors
-  
-  // Choose a random color from colors[]
-  const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-  // Set the background color of body
-  document.body.style.backgroundColor = randomColor;
-}
-
-// Updates updateCount function every second
-setInterval(updateCount, 1000);
+    // Updates updateCount function every second
+    setInterval(updateCount, 1000);
