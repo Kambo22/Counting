b6bc799e735bea7eff3dbb208a7bdc20f0c9bde0
Kambo22 <anikolov432@gmail.com>
2024-04-07 16:13:08 +0300

    i dont know actualy

---
+++ draft.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/3lMSr8PsP5KQaAht/scene.splinecode');
